ffaf84d6f4de8e1c4ae598b3d007271e5147e927
shubhamsk07 <shubhamkarmyal95@gmail.com>
2024-08-31 05:34:14 +0530

    added div and p

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,10 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen">
       <div className="border hover:border-slate-900 rounded">
+        
         <WalletMultiButton style={{}} />
+        <div>Web Based wallet (DCEX) ADAPTER</div>
+        <p>Currently dosen't support transaction </p>
       </div>
     </main>
   );
